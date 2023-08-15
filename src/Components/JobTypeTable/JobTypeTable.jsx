867d01216e4b6705f1588338f61a8f686c0d988c
AcedenD <adenddang@gmail.com>
2023-08-15 16:49:49 +1000

    change Services to services in import

--- src/Components/JobTypeTable/JobTypeTable.jsx
+++ src/Components/JobTypeTable/JobTypeTable.jsx
@@ -1,45 +1,47 @@
-import React, { useRef, useState } from 'react'
-import { Space, Table, Tag } from 'antd';
-import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { jobTypeServ } from '../../Services/jobTypeServices';
-import { getAllJobType } from '../../redux/slices/jobTypeSlice';
+import React, { useRef, useState } from "react";
+import { Space, Table, Tag } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { jobTypeServ } from "../../services/jobTypeServices";
+import { getAllJobType } from "../../redux/slices/jobTypeSlice";
 
 const JobTypeTable = () => {
-const { jobTypeData } = useSelector((state) => state.jobTypes);
+  const { jobTypeData } = useSelector((state) => state.jobTypes);
   const dispatch = useDispatch();
   const ref = useRef();
 
-//   const shortenText = (text, maxLength) => {
-//     if (text.split(' ').length > maxLength) {
-//       const words = text.split(' ');
-//       return words.slice(0, maxLength).join(' ') + '...';
-//     }
-//     return text;
-//   };
+  //   const shortenText = (text, maxLength) => {
+  //     if (text.split(' ').length > maxLength) {
+  //       const words = text.split(' ');
+  //       return words.slice(0, maxLength).join(' ') + '...';
+  //     }
+  //     return text;
+  //   };
 
   const columns = [
     {
-      title: 'ID',
-      dataIndex: 'id',
-      key: 'id',
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
       render: (text) => <a>{text}</a>,
     },
     {
-      title: 'Job Type',
-      dataIndex: 'jobType',
-      key: 'jobType',
-
+      title: "Job Type",
+      dataIndex: "jobType",
+      key: "jobType",
     },
     {
-      title: 'Action',
-      key: 'action',
+      title: "Action",
+      key: "action",
       render: (_, record) => (
         <Space size="middle">
-          <button className='py-2 px-5 bg-red-600 text-white rounded-lg hover:bg-red-700 suration-500'
+          <button
+            className="py-2 px-5 bg-red-600 text-white rounded-lg hover:bg-red-700 suration-500"
             title="Xóa"
             onClick={() => {
-              const userConfirmed = window.confirm("Do you really want to delete?");
+              const userConfirmed = window.confirm(
+                "Do you really want to delete?"
+              );
               if (userConfirmed) {
                 jobTypeServ
                   .deleteJobType(record.jobType)
@@ -52,7 +54,10 @@ const { jobTypeData } = useSelector((state) => state.jobTypes);
                     alert("There is a problem deleting");
                   });
               }
-            }}><i class="fa-solid fa-trash-can"></i></button>
+            }}
+          >
+            <i class="fa-solid fa-trash-can"></i>
+          </button>
           {/* <NavLink
             to={`/admin/jobType/edit?jobTypeId=${record.jobType}`} 
             className='py-2 px-5 bg-yellow-600 text-white rounded-lg hover:bg-yellow-700 suration-500'
@@ -70,9 +75,7 @@ const { jobTypeData } = useSelector((state) => state.jobTypes);
     id: index + 1,
   }));
 
-  return (
-    <Table columns={columns} dataSource={filteredAndMappedJobTypes} />
-  )
-}
+  return <Table columns={columns} dataSource={filteredAndMappedJobTypes} />;
+};
 
-export default JobTypeTable
+export default JobTypeTable;
