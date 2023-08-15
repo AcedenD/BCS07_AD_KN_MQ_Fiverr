1f2c6a32422b1933d00cee3483ef3abad14d54e2
AcedenD <adenddang@gmail.com>
2023-08-15 22:13:59 +1000

    updated jobdetail

--- src/pages/JobDetail/JobDetail.jsx
+++ src/pages/JobDetail/JobDetail.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { congViecServ } from "../../services/congViecServices";
 import "./JobDetail.scss";
+import JobDetailCard from "../../Components/JobDetailCard/JobDetailCard";
 
 const JobDetail = () => {
   const { id } = useParams();
@@ -33,6 +34,7 @@ const JobDetail = () => {
     <div className="max-w-screen-xl mx-5 lg:mx-auto mt-32 py-2 grid grid-cols-3 gap-5 ">
       <div className="jobDetail_left border border-red-400 col-span-2 min-h-screen p-10 ">
         <Breadcrumb
+          className={"breadcrumb text-lg font-semibold mb-5"}
           separator=">"
           items={[
             {
@@ -46,39 +48,12 @@ const JobDetail = () => {
             },
           ]}
         />
-        <div className="flex flex-col">
-          <h1 className="jobTitle text-3xl font-bold ">
-            {congViec.tenCongViec}
-          </h1>
-          <div className="seller_overview flex flex-row items-center gap-5 py-2">
-            <div className="seller_avatar w-[36px] h-[36px]">
-              <img className="rounded-full" src={avatar} alt="" />
-            </div>
-            <div className="seller_name ">
-              <p className="text-md font-semibold">
-                {tenNguoiTao.charAt(0).toUpperCase() + tenNguoiTao.slice(1)}
-              </p>
-            </div>
-            <div className="seller_level">
-              <p className="text-md text-gray-500">
-                Level {congViec.saoCongViec} Seller
-              </p>
-            </div>
-            <div className="seller_star_rating flex ">
-              <div className="stars">
-                <i className="fa-solid fa-star text-yellow-400 "></i>
-                <i className="fa-solid fa-star text-yellow-400"></i>
-                <i className="fa-solid fa-star text-yellow-400"></i>
-                <i className="fa-solid fa-star text-gray-300"></i>
-                <i className="fa-solid fa-star text-gray-300"></i>
-                <span className="mx-3 text-yellow-500">
-                  {congViec.saoCongViec}
-                </span>
-                <span className="text-gray-500">({congViec.danhGia})</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <JobDetailCard
+          congViec={congViec}
+          avatar={avatar}
+          tenNguoiTao={tenNguoiTao}
+          tenChiTietLoai={tenChiTietLoai}
+        />
       </div>
       <div className="jobDetail_right border border-blue-400 col-span-1 max-h-96 p-5">
         testing
