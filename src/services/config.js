b8bc9ffa1e3d7fc0b0d4509250efa6ecb56c1669
AcedenD <adenddang@gmail.com>
2023-08-24 12:59:33 +0700

    change accesstoken from local

--- src/services/config.js
+++ src/services/config.js
@@ -7,12 +7,10 @@ const BASE_URL = "https://fiverrnew.cybersoft.edu.vn";
 const tokenCybersoft =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCBTw6FuZyAwNyIsIkhldEhhblN0cmluZyI6IjE5LzEyLzIwMjQiLCJIZXRIYW5UaW1lIjoiMjcwMjk0NDAwMDAwMCIsIm5iZiI6MTY3OTg1MDAwMCwiZXhwIjoyNzAzMDkxNjAwfQ.WhiaWv7nPOi0RVoZB4Yckidm1CYBy-8xeDVf55p4DGw";
 
-// const tokenAuthorization = layDuLieuLocal("access_token");
 // for testing
-const tokenAuthorization =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI2NTIiLCJlbWFpbCI6InRlc3RlcjFAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwibmJmIjoxNjkwNjM5MzcxLCJleHAiOjE2OTEyNDQxNzF9.YbZiTWHYODpwkiOErI315HMVUAZoDd-2FVffPud-kRY";
+// const tokenAuthorization = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjI2NTIiLCJlbWFpbCI6InRlc3RlcjFAZ21haWwuY29tIiwicm9sZSI6IkFETUlOIiwibmJmIjoxNjkwNjM5MzcxLCJleHAiOjE2OTEyNDQxNzF9.YbZiTWHYODpwkiOErI315HMVUAZoDd-2FVffPud-kRY";
 // real code below
-// const tokenAuthorization = layDuLieuLocal("access_token");
+const tokenAuthorization = layDuLieuLocal("token");
 
 // console.log(tokenAuthorization);
 
